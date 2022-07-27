23bc69109232cacf443f2a5eebafa387817b1b2f
Luciano Ortiz <lucianoortizsilva@gmail.com>
2022-07-27 00:56:45 -0300

    add GET /cep/{codigo}

--- cep-api/go.mod
+++ cep-api/go.mod
@@ -2,4 +2,7 @@ module github.com/lucianoortizsilva/cep/cep-api
 
 go 1.18
 
-require github.com/joho/godotenv v1.4.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+)
--- cep-api/main.go
+++ cep-api/main.go
@@ -5,10 +5,13 @@ import (
 	"net/http"
 
 	"github.com/lucianoortizsilva/cep/cep-api/src/config"
+	"github.com/lucianoortizsilva/cep/cep-api/src/router"
 )
 
 func main() {
 	config.InicializarVariaveisAmbiente()
+	rotas := router.InicializarRotas()
+
 	fmt.Printf("CEP-API Iniciado na porta: %d\n", config.PORT_HTTP)
-	http.ListenAndServe(fmt.Sprintf(":%d", config.PORT_HTTP), nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", config.PORT_HTTP), rotas)
 }
